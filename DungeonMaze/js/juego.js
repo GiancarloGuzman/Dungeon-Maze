6c9e45cb8ffb6730bf2c44d805d674adfaa8eb12
GiancarloGuzman <giancarlojuanes63@hotmail.com>
2011-11-10 17:25:37 -0500

    Update DungeonMaze/js/juego.js

--- DungeonMaze/js/juego.js
+++ DungeonMaze/js/juego.js
@@ -1,4 +1,4 @@
-var y = 0;
+var y = 300;
 var x = 0;
 var y_troll = 100;
 var x_troll = 300;
@@ -25,31 +25,48 @@ function detectarColision() {
     }
 }
 
+function arriba(){
+    	y = y - 50;
+        if (y < 0) y = 0;
+        $("#pedobear").css({ "top": y + "px"});
+}
+function abajo(){
+		y = y + 50;
+        if (y > 550) y = 600;
+        $("#pedobear").css({ "top": y + "px"});
+}
+function derecha(){
+		x = x + 50;
+        if (x > 1150) x = 1200;        
+        $("#pedobear").css({ "left": x + "px"});
+}
+function izquierda(){
+		x = x - 50;
+        if (x < 0) x = 0;
+        $("#pedobear").css({ "left": x + "px"});
+}
+
 $("body").keypress(function(e) {
     if (e.which == 115) {
         //DOWN - S
-        y = y + 5;
-        if (y > 370) y = 370;
-        $("#pedobear").css({ "top": y + "px"});
+        abajo();
+		cambiarPosicionTroll();
       
     }
     if (e.which == 119) {
         //UP - W
-        y = y - 5;
-        if (y < 0) y = 0;
-        $("#pedobear").css({ "top": y + "px"});
+		arriba();
+		cambiarPosicionTroll();
     }
     if (e.which == 100) {
         //RIGHT - D
-        x = x + 5;
-        if (x > 410) x = 410;        
-        $("#pedobear").css({ "left": x + "px"});
+        derecha();
+		cambiarPosicionTroll();
     }
     if (e.which == 97) {
         //LEFT - A
-        x = x - 5;
-        if (x < 0) x = 0;
-        $("#pedobear").css({ "left": x + "px"});
+        izquierda();
+		cambiarPosicionTroll();
     }
     detectarColision();
 });
