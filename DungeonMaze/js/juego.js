282078c229d8c1693a942ea8b633a22af8d7511e
Giancarlo Guzman <giancarlojuanes63@hotmail.com>
2011-11-15 17:29:55 -0500

    actualizacion

--- DungeonMaze/js/juego.js
+++ DungeonMaze/js/juego.js
@@ -1,8 +1,11 @@
 var y = 300;
 var x = 0;
-var y_orco = 100;
-var x_orco = 300;
-/*var interval_id = setInterval(function() { cambiarPosicionOrco(); },3000);*/
+var y_orco = 150;
+var x_orco = 150;
+var y_orco1 = 250;
+var x_orco1 = 250;
+var y_orco2 = 350;
+var x_orco2 = 350;
 
 function cambiarPosicionOrco() {
     var aleatorio = Math.floor(Math.random()*4);
@@ -17,13 +20,8 @@ function cambiarPosicionOrco() {
 	}
 	else if(aleatorio==3){
 	izquierdaOrco();
-	}
-	
+	}	
 }
-
-$(document).ready(function () {
-    cambiarPosicionOrco();
-});
 
 function detectarColision() {
     if (x == x_orco && y == y_orco) {
@@ -59,23 +57,46 @@ function arribaOrco(){
 		y_orco = y_orco - 50;
         if (y_orco < 0) y_orco = 0;
         $("#trollface").css({ "top": y_orco + "px"});
+		y_orco1 = y_orco1 - 50;
+        if (y_orco1 < 0) y_orco1 = 0;
+        $("#trollface1").css({ "top": y_orco1 + "px"});
+		y_orco2 = y_orco2 - 50;
+        if (y_orco2 < 0) y_orco2 = 0;
+        $("#trollface2").css({ "top": y_orco2 + "px"});
 }
 function abajoOrco(){
 		y_orco = y_orco + 50;
         if (y_orco > 550) y_orco = 600;
         $("#trollface").css({ "top": y_orco + "px"});
+		y_orco1 = y_orco1 + 50;
+        if (y_orco1 > 550) y_orco1 = 600;
+        $("#trollface1").css({ "top": y_orco1 + "px"});
+		y_orco2 = y_orco2 + 50;
+        if (y_orco2 > 550) y_orco2 = 600;
+        $("#trollface2").css({ "top": y_orco2 + "px"});
 }
 function derechaOrco(){
 		x_orco = x_orco + 50;
         if (x_orco > 1050) x_orco = 1100;        
         $("#trollface").css({ "left": x_orco + "px"});
+		x_orco1 = x_orco1 + 50;
+        if (x_orco1 > 1050) x_orco1 = 1100;        
+        $("#trollface1").css({ "left": x_orco1 + "px"});
+		x_orco2 = x_orco2 + 50;
+        if (x_orco2 > 1050) x_orco2 = 1100;        
+        $("#trollface2").css({ "left": x_orco2 + "px"});
 }
 function izquierdaOrco(){
 		x_orco = x_orco - 50;
         if (x_orco < 0) x_orco = 0;
         $("#trollface").css({ "left": x_orco + "px"});
+		x_orco1 = x_orco1 - 50;
+        if (x_orco1 < 0) x_orco1 = 0;
+        $("#trollface1").css({ "left": x_orco1 + "px"});
+		x_orco2 = x_orco2 - 50;
+        if (x_orco2 < 0) x_orco2 = 0;
+        $("#trollface2").css({ "left": x_orco2 + "px"});
 }
-
 $("body").keypress(function(e) {
     if (e.which == 115) {
         //DOWN - S
